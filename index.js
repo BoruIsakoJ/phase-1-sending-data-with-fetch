e2cf23dc5d6d576fc1e7553426ff9b5138658c72
Boru Isako <isco.jaldy@gmail.com>
2025-03-20 09:05:00 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(resp => resp.json())
+        .then(data => {
+            const bodyEl = document.querySelector('body')
+            const divEl = document.createElement('div')
+            divEl.innerHTML =`<p>${data.id}</p>`
+            bodyEl.appendChild(divEl)
+            
+        })
+
+        .catch(function (error) {        
+            const bodyEl = document.querySelector('body')
+            const divEl = document.createElement('div')
+            divEl.innerHTML =`<p>${error.message}</p>`
+            bodyEl.appendChild(divEl)
+            console.log(error.message);
+          });
+}
+
+submitData("Boru", "isco.jaldy@gmail.com")
